89221a5c204a69898e446d47d198a4185d97f775
mrcamilletti <mrcamilletti@gmail.com>
2021-04-24 10:12:05 -0300

    bugfix: Multiple calls to addTask. Serial tasks being executed in parallel timers corrected.

--- timerQueue.js
+++ timerQueue.js
@@ -6,46 +6,59 @@ Original code
 Code modified to set a maximum number of tasks and compatible with promises.
 */
 
+const nodeRed = require("node-red");
+
 module.exports = TimerQueue;
 
 function TimerQueue(capacity = 128){
+    this.running = false;
     this.currentTimer = null;
     this.tasks = [];
     this._capacity = capacity;
+    this.debug_mode = false;
   }
   
 TimerQueue.prototype.addTask = function(callback, delay){
-  if (this.tasks.length < this._capacity) {
-    this.tasks.push({ callback: callback, delay: delay });
+  var self = this;
+
+  if (self.tasks.length < self._capacity) {
+    self.tasks.push({ callback: callback, delay: delay });
   } else {
     return false;
   }
 
   // If there's a scheduled task, bail out.
-  if(this.currentTimer) return true;
+  if(self.running == true) return true;
 
   // Otherwise, start kicking tires
-  this.launchNextTask();
+  self.running = true;
+  self.launchNextTask();
   return true;
 };
 
 TimerQueue.prototype.launchNextTask = function(){
 
   // If there's a scheduled task, bail out.
-  if(this.currentTimer) return;
+  //if(this.running == true) return;
+  
 
   var self = this;
   var nextTask = self.tasks.shift();
 
   // There's no more tasks, clean up.
-  if(!nextTask) return this.clear();
+  if(!nextTask) {
+    self.running = false;
+    return false;
+  }  
+
 
   // Otherwise, schedule the next task.
   nextTask.callback( () => {
+    if (self.debug_mode) console.log("Task launched");
     self.currentTimer = setTimeout(function(){
-      self.currentTimer = null;
       // Call this function again to set up the next task.
-      self.launchNextTask();
+      if (self.debug_mode) console.log("Next task ...");
+      self.launchNextTask();      
     }, nextTask.delay);
   });
 };
@@ -53,13 +66,15 @@ TimerQueue.prototype.launchNextTask = function(){
 TimerQueue.prototype.clear = function(){
   var self = this;
 
-  if (self.currentTimer) clearTimeout(self.currentTimer);
-
-  // Timer clears only destroy the timer. It doesn't null references.
-  self.currentTimer = null;
-
+  if (self.running) { 
+    clearTimeout(self.currentTimer);
+    // Timer clears only destroy the timer. It doesn't null references.
+    self.running = false;
+  }
+  
   // Fast way to clear the task queue
   //this.tasks.length = 0;
+  self.tasks.splice(0,self.tasks.length);
 };
 
 TimerQueue.prototype.length = function() {
